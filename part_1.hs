6d86b1df51dde1b11bbb38b05a438e9844092cb3
andrew.tseng <andrew.tseng@innolux.com>
2024-08-08 17:19:42 +0800

    Part 1 start

---
+++ part_1.hs
@@ -0,0 +1,18 @@
+stepFuncton :: Double -> Double
+stepFuncton x = if x <= 0 then 0 else 1
+
+type R = Double
+
+type Time         = R
+type TimeInterval = R
+type Position     = R
+type Velocity     = R
+
+type PositionFunction = Time -> Position
+type VelocityFunction = Time -> Velocity
+
+avgVelocity1 :: Time -> Time -> PositionFunction -> Velocity
+avgVelocity1 t0 t1 x = (x t1 - x t0) / (t1 - t0)
+
+avgVelocity2 :: Time -> TimeInterval -> PositionFunction -> Velocity
+avgVelocity2 t dt x = (x (t + dt/2) - x (t - dt/2)) / dt
